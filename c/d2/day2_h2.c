b62f973e14838dcb33fb645a301a2af8434f783c
Sachin Kumar <sachinkr432@gmail.com>
2020-08-15 21:44:39 +0530

    New Sem Starting

--- c/d2/day2_h2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{
-	float num=34.5678;
-	printf("The number is %f\n",num );
-	printf("Integer rounding is %.0f\n",num );
-	printf("Rounded to 2 decimal is %.2f\n",num );
-}
